561683089c39cf85b8fadce94b0dcab90d285b8a
Kiisi <kiisifelix06@gmail.com>
2023-07-19 08:33:24 +0100

    Feat - Faqs form search input

--- src/pages/Faqs.jsx
+++ src/pages/Faqs.jsx
@@ -1,19 +1,29 @@
 import Footer from "../layout/Footer"
 import Navbar from "../layout/Navbar"
 import faqs from '../assets/faqs.svg'
+import Button from "../components/Button"
 
 const Faqs = () => {
   return (
     <>
             <Navbar />
             <main className='px-4 sm:px-6 bg-dark pt-[5rem] pb-[14rem]'>
-                <div className='max-w-[800px] mx-auto grid place-items-center'>
-                    <header className='pt-10'>
+                <div className='max-w-[800px] w-full mx-auto grid place-items-center'>
+                    <header className='pt-10 pb-20'>
                         <figure>
                             <h1 className='text-center text-[24px] md:text-[32px] pb-12 text-white font-bold'>FAQs</h1>
                             <img src={faqs} alt="Frequently Asked Questions" className='w-full max-w-[429px]' />
                         </figure>
                     </header>
+                    <div className='w-full'>
+                        <form className='flex w-full'>
+                            <fieldset className='flex-1 bg-[#fff] px-3 flex items-center gap-x-3'>
+                                <span className="material-icons text-[#aaa]"> search </span>
+                                <input className='w-full outline-none' placeholder='Ask a question' />
+                            </fieldset>
+                            <Button className='rounded-none'>Search</Button>
+                        </form>
+                    </div>
                 </div>
             </main>
             <Footer />
